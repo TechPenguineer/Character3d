596d449229660129666622ea959e0234172c6834
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-25 22:18:19 -0400

    create window properties

--- Engine/Window.cpp
+++ Engine/Window.cpp
@@ -1,11 +1,35 @@
 #include "Window.h"
 
+Window::Window()
+{
+
+}
+
 bool Window::init()
 {
-	return false;
+	WNDCLASSEX wc;
+	wc.cbClsExtra = NULL;
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.cbWndExtra = NULL;
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = NULL;
+	wc.lpszClassName = L"MyWindowclass";
+	wc.lpszMenuName = L"";
+	wc.style = NULL;
+
+	if(!::RegisterClassEx(&wc))
+	{
+		return false;
+	}
+	::CreateWindowEx(WS_EX_OVERLAPPEDWINDOW, L"MyWindowClass", L"Character 3D", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 1024, 768, NULL, NULL, NULL,NULL);
+
+	return true;
 }
 
 bool Window::release()
 {
-	return false;
+	return true;
 }
